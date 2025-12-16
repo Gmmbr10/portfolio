16e0689e8f3053b4bd148baf876990d6c32dc9ed
giovanne <giovannemmelo@gmail.com>
2025-12-16 20:13:06 -0300

    feat: create card component

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,7 @@
+import style from './card.module.css';
+
+export function Card({children,className,...props}){
+    return (
+        <div className={`${style.card} ${className}`} {...props}>{children}</div>
+    )
+}
