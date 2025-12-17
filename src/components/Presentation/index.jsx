cb7d0e4ad7485cd804c24abc4067874cdde796b2
giovanne <giovannemmelo@gmail.com>
2025-12-17 17:44:20 -0300

    repair: adjust the content

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -3,14 +3,16 @@ import style from "./presentation.module.css";
 export function Presentation() {
 	return (
 		<section className={`${style.container}`}>
-			<h2>Hey There!</h2>
+			<h1>Hi, I'm Giovanne Monteiro 👋🏽</h1>
 			<div className={`${style.content}`}>
 				<p>
-					I'm Giovanne a future{" "}
-					<span className={`${style.mark}`}>Software Developer</span>
+					<span className={`${style.mark}`}>Software Developer</span> based in
+					Brazil.
 				</p>
-				<p>I'm based on Brazil and looking for a job</p>
-				<p>I'm curious, so I'm always studying something (mainly IT)</p>
+				<p>
+					I turn real-world problems into scalable backend systems and clean web
+					applications.
+				</p>
 			</div>
 		</section>
 	);
