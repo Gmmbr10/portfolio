57df3d525e7aed541dedda90b7aa25197ae07c4f
giovanne <giovannemmelo@gmail.com>
2025-12-17 18:30:22 -0300

    repair: refactoring content and styling

--- src/components/Presentation/index.jsx
+++ src/components/Presentation/index.jsx
@@ -3,15 +3,21 @@ import style from "./presentation.module.css";
 export function Presentation() {
 	return (
 		<section className={`${style.container}`}>
-			<h1>Hi, I'm Giovanne Monteiro 👋🏽</h1>
-			<div className={`${style.content}`}>
-				<p>
-					<span className={`${style.mark}`}>Software Developer</span> based in
-					Brazil.
+			<div className={`${style.imageBox}`}>
+				<img
+					src="https://github.com/gmmbr10.png"
+					className={`${style.image}`}
+				/>
+			</div>
+			<div className={`${style.description}`}>
+				<h1 className={`${style.title}`}>Hi, I'm Giovanne Monteiro 👋🏽</h1>
+				<p className={`${style.highlight}`}>
+					Fullstack<br/>
+					Developer
 				</p>
 				<p>
-					I turn real-world problems into scalable backend systems and clean web
-					applications.
+					I'm a fullstack developer based in Brazil, I turn real-world problems
+					into scalable backend systems and clean web applications.
 				</p>
 			</div>
 		</section>
